c26d224756eb4b12587389b40bad39b40e0875a4
Ngo Thi Kim Ly <ntkl1710@gmail.com>
2024-06-07 20:43:59 +0700

    fix login.js

--- template/js/login.js
+++ template/js/login.js
@@ -5,7 +5,7 @@ function login() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
     let count = 0;
-    var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+*$/;
     if (!mailformat.test(username) || username == ""){
         p1.innerText = 'Please check your username'
         p1.classList.add('active')
@@ -19,36 +19,7 @@ function login() {
     }
     else p2.classList.remove('active');
     
-        if (count == 0) {
-            fetch('https://recruitment-api.pyt1.stg.jmr.pl/login', {
-                method: 'POST', //GET
-                headers: { 'Content-Type': 'application/json'},
-                body: JSON.stringify({
-                    login: username,
-                    password: password
-                })
-            })
-                .then(response => {
-                    if (response.ok) {
-                        return response.json(); // Chuyển đổi phản hồi JSON thành đối tượng JavaScript
-                    } else {
-                        throw new Error('Network response was not ok');
-                    }
-                })
-                .then(data => {
-                    // Xử lý phản hồi thành công
-                    if (data.status === 'ok') {
-                        window.location.href = 'http://127.0.0.1:5500/homePage/homePage.html';
-                    } else {
-                        p2.innerText = 'Your account and password are invalid'
-                        p2.classList.add('active')  
-                    }
-                })
-                .catch(error => {
-                    // Xử lý lỗi từ máy chủ
-                    console.error('Error:', error);
-                });
-        }
+  
     }
 
 document.addEventListener("DOMContentLoaded", function() {
