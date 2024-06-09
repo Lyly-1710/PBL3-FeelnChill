aa0c924a623a542dbf528c87c78c0977b7101de7
Ngo Thi Kim Ly <ntkl1710@gmail.com>
2024-06-09 23:10:44 +0700

    xong login regis

--- template/js/login.js
+++ template/js/login.js
@@ -1,41 +1,53 @@
-var p1 = document.querySelector('.email_error');
-var p2 = document.querySelector('.password_error');
+// var p1 = document.querySelector('.email_error');
+// var p2 = document.querySelector('.password_error');
 
-function login() {
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-    let count = 0;
-    var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+*$/;
-    if (!mailformat.test(username) || username == ""){
-        p1.innerText = 'Please check your username'
-        p1.classList.add('active')
-        count++;
-    }
-    else p1.classList.remove('active');
-    if (password == ""){
-        p2.innerText = 'Please enter your password'
-        p2.classList.add('active')
-        count++;
-    }
-    else p2.classList.remove('active');
-    }
+// function login() {
+//     const username = document.getElementById("username").value;
+//     const password = document.getElementById("password").value;
+//     let count = 0;
+//     var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+*$/;
+//     if (!mailformat.test(username) || username == ""){
+//         p1.innerText = 'Please check your username'
+//         p1.classList.add('active')
+//         count++;
+//     }
+//     else p1.classList.remove('active');
+//     if (password == ""){
+//         p2.innerText = 'Please enter your password'
+//         p2.classList.add('active')
+//         count++;
+//     }
+//     else p2.classList.remove('active');
+//     }
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("Login-button").addEventListener("click", login)
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//     document.getElementById("Login-button").addEventListener("click", login)
+// });
 
-function loginDisplay() {
-    window.location.href = "homePage.html";
-}
+// function loginDisplay() {
+//     window.location.href = "homePage.html";
+// }
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("Login").addEventListener("click", loginDisplay)
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//     document.getElementById("Login").addEventListener("click", loginDisplay)
+// });
 
-//register
-function regis()
-{
-    document.getElementByClassName("regis-form").style.display = "block";
-    document.getElementByClassName("login-form").style.display = "none";
+// //register
+// function regis()
+// {
+//     document.getElementByClassName("regis-form").style.display = "block";
+//     document.getElementByClassName("login-form").style.display = "none";
 
+// }
+let create = document.getElementById('create');
+create.addEventListener('click', regis);
+
+let login = document.getElementsByClassName("content")[0];
+let reg = document.getElementsByClassName('regis-form')[0];
+let reg_btn = document.getElementsByClassName('register-button')[0];
+function regis() {
+
+    login.style.display = "none";
+
+    reg.style.display = "block";
 }
